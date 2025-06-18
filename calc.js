464e2152b09d27b1971a21fe928d2235cb427c87
jaganyv <jaganyv1@gmail.com>
2025-06-18 11:23:36 +0530

    Commited

---
+++ calc.js
@@ -0,0 +1,15 @@
+let num1 = 10;
+let num2 = 20;
+let sum = num1+num2;
+let diff = num1-num2;
+let product = num1*num2;
+let division = num1/num2;
+console.log(sum,diff,product,division);
+
+let number = 20;
+if(number%2==0){
+  console.log("This is Even Number!");
+}
+else{
+    console.log("This is Odd Number!");
+}
